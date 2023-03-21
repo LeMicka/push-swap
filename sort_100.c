348f0432762640715c36055d643109b97f5cca92
Mickael Bruzzi <mbruzzi@c3r4s5.42malaga.com>
2023-03-21 12:21:12 +0100

    sort100

--- sort_100.c
+++ sort_100.c
@@ -6,20 +6,64 @@
 /*   By: mbruzzi <mbruzzi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 09:27:36 by mbruzzi           #+#    #+#             */
-/*   Updated: 2023/03/09 14:33:27 by mbruzzi          ###   ########.fr       */
+/*   Updated: 2023/03/21 11:23:34 by mbruzzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	pre_sort(t_bounds *b)
+/* void	pre_sort_first(t_bounds *a, t_bounds *b)
 {
 	if (b->head != NULL && b->head->next != NULL)
 	{
-		if (b->head->order > b->head->next->order)
-			make_sb(b->head);
-		else if (b->head->order < b->last->order)
+		if (a->head->order > a->last->order && b->head->order < b->last->order)
+		{
+			make_rra(a);
 			make_rb(b);
+		}	
+		if (b->head->order < b->head->next->order)
+			make_sb(b->head);
+	}
+}
+
+void	sort_and_push(t_bounds *stack_a, t_bounds *stack_b, int elem, int pt)
+{
+	int	i;
+
+	i = 0;
+
+	if (pt == 1 && elem < 2)
+		make_pb(stack_a, stack_b);
+	else
+	{
+		if (stack_a->head->order == stack_b->head->order - 1
+			|| stack_a->head->order == stack_b->head->order + 1)
+			make_pb(stack_a, stack_b);
+		else if (stack_a->head->order == stack_b->last->order - 1
+			|| stack_a->head->order == stack_b->last->order + 1)
+		{
+			make_pb(stack_a, stack_b);
+			make_rb(stack_b);
+		}
+		else
+			make_pb(stack_a, stack_b);
+	}
+}
+
+void	push_last(t_var *vars, t_bounds *a, t_bounds *b, int pt)
+{
+	int	i;
+
+	i = 0;
+	while (i < (vars->len / vars->parts))
+	{
+		if (a->head->order <= (vars->len) - 3)
+		{
+			sort_and_push(a, b, i, pt);
+			i++;
+		}		
+		else
+			make_ra(a);
 	}
 }
 
@@ -28,44 +72,34 @@ void	push_to_b(t_var *vars, t_bounds *stack_a, t_bounds *stack_b, int pt)
 	int	i;
 
 	i = 0;
-	if (pt == vars->parts)
+	while (i < (vars->len / vars->parts) - 1)
 	{
-		while (i < (vars->len / vars->parts) - 3)
+		if (stack_a->head->order <= (vars->len / vars->parts) * pt - 1)
 		{
-			if (stack_a->head->order <= (vars->len))
-				make_pb(stack_a, stack_b);
-			else
-				make_ra(stack_a);
-			pre_sort(stack_b);
+			sort_and_push(stack_a, stack_b, i, pt);
+			if (pt == 1)
+				pre_sort_first(stack_a, stack_b);
 			i++;
-		}
+		}		
+		else
+			make_ra(stack_a);
 	}
-	else
-	{
-		while (i < (vars->len / vars->parts))
-		{
-			ft_printf("q= %i\n", ((vars->len / vars->parts) * pt));
-			if (stack_a->head->order <= (vars->len / vars->parts) * pt)
-			{
-				make_pb(stack_a, stack_b);
-				pre_sort(stack_b);
-				i++;
-			}		
-			else
-				make_ra(stack_a);
-		}
-	}
-}
+} */
+
+
 
 void	sort_100(t_var *vars, t_bounds *stack_a, t_bounds *stack_b)
 {
 	int	i;
 
 	i = 1;
-	while (i < 4)
+	while (i < 6)
 	{
-		ft_printf("i= %i\n", i);
-		push_to_b(vars, stack_a, stack_b, i);
+		if (i == 5)
+			push_last(vars, stack_a, stack_b, i);
+		else
+			push_to_b(vars, stack_a, stack_b, i);
 		i++;
 	}
+	sort_3(stack_a);
 }
